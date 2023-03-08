c72dd0179ce94725f1963ff318c25b1db1fdd022
WesBourkeMusic <wesbourkeavl@gmail.com>
2023-03-08 01:24:25 -0500

    Added React Router and checked out some of the new features

--- climbing-front-end/src/index.js
+++ climbing-front-end/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import Root from "./routes/Root"
+import ReactRouterErrorPage from "./errorMessages/ReactRouterErrorPage";
+import Contact from "./routes/Contact";
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ReactRouterErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} /> 
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
