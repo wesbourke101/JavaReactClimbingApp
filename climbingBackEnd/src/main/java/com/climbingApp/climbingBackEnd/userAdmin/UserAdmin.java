76a84222c9ab0d98df912e867be43b1b33494b5d
wesbourke <wesbourkeavl@gmail.com>
2023-03-09 14:09:46 -0500

    Pushing what I have so I can start to work on my windows.

--- climbingBackEnd/src/main/java/com/climbingApp/climbingBackEnd/userAdmin/UserAdmin.java
+++ climbingBackEnd/src/main/java/com/climbingApp/climbingBackEnd/userAdmin/UserAdmin.java
@@ -1,12 +1,21 @@
 package com.climbingApp.climbingBackEnd.userAdmin;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.UUID;
 
 @Entity
 @Table(name = "user_admin")
 public class UserAdmin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "user_name")
+    private String userName;
+
 
 
 }
